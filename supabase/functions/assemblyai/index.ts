ea9516a4637264608ba68d191dcf5a00f551606e
Epidemy <aipdimi@gmail.com>
2023-12-08 22:59:50 +0330

    add: call from browser added

--- supabase/functions/assemblyai/index.ts
+++ supabase/functions/assemblyai/index.ts
@@ -2,18 +2,32 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log("Hello from Functions!")
+import { corsHeaders } from '../_shared/cors.ts'
+
+console.log(`Function "browser-with-cors" up and running!`)
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+  try {
+    const { name } = await req.json()
+    const data = {
+      message: `Hello ${name}!`,
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
 })
 
 /* To invoke locally:
